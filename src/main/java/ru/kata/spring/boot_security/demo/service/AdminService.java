8543861db51a14648e3233de020ade68bf039b6a
Vladislav Kolesov <AkcTucb1@gmail.com>
2025-04-16 21:29:04 +0300

    Вот

--- src/main/java/ru/kata/spring/boot_security/demo/service/AdminService.java
+++ src/main/java/ru/kata/spring/boot_security/demo/service/AdminService.java
@@ -1,14 +1,23 @@
 package ru.kata.spring.boot_security.demo.service;
 
+import ru.kata.spring.boot_security.demo.dto.UserDTO;
 import ru.kata.spring.boot_security.demo.model.User;
 
 import java.util.List;
 
 public interface AdminService {
+
     List<User> getAllUsers();
+
+    /*  Новые сигнатуры, принимающие DTO  */
+    void addUser(UserDTO dto);
+    void update(UserDTO dto);
+
+    /*  Прежние методы можно оставить,
+        если их использует остальной код  */
     void addUser(User user);
-    void deleteUser(Long id);
     void update(User user);
 
+    void deleteUser(Long id);
     User findById(Long id);
 }
